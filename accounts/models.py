40db4c109f5a5b373cba09107909c8aad91aa642
Fatima Ibrahim <162383996+Fatima7770@users.noreply.github.com>
2026-05-12 23:10:21 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,14 @@ from django.contrib.auth.models import User, AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=15, blank=True,null=True)
+    from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+    status = models.CharField(max_length=20, default="available")
+
+    def __str__(self):
+        return self.title
